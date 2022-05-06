f7aed2e7f634655e1bd7b7b8145f1378eb6be30e
Serega-HTMLchik <70023791+Serega-HTMLchik@users.noreply.github.com>
2022-05-06 21:27:45 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header/Header';
+import Body from './components/body/Body';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
